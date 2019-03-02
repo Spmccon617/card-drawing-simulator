9d1aeefab084877ca430993847d90e220b010d02
Sean McConnell <47701446+Spmccon617@users.noreply.github.com>
2019-03-02 13:42:11 -0500

    Update code.py

--- code.py
+++ code.py
@@ -4,6 +4,111 @@ while True:
     card = randint(1,52)
     drawCard = input('Do you want to draw a card?')
     
+    if card == 1:
+        print('Ace of spades')
+    elif card == 2:
+        print('2 of spades')
+    elif card == 3:
+        print('3 of spades')
+    elif card == 4:
+        print('4 of spades')
+    elif card == 5:
+        print('5 of spades')
+    elif card == 6:
+        print('6 of spades')
+    elif card == 7:
+        print('7 of spades')
+    elif card == 8:
+        print('8 of spades')
+    elif card == 9:
+        print('9 of spades')
+    elif card == 10:
+        print('10 of spades')
+    elif card == 11:
+        print('Jack of spades')
+    elif card == 12:
+        print('Queen of spades')
+    elif card == 13:
+        print('King of spades')
+    elif card == 14:
+        print('Ace of diamonds')
+        elif card == 15:
+        print('2 of diamonds')
+    elif card == 16:
+        print('3 of diamonds')
+    elif card == 17:
+        print('4 of diamonds')
+    elif card == 18:
+        print('5 of diamonds')
+    elif card == 19:
+        print('6 of diamonds')
+    elif card == 20:
+        print('7 of diamonds')
+    elif card == 21:
+        print('8 of diamonds')
+    elif card == 22:
+        print('9 of diamonds')
+    elif card == 23:
+        print('10 of diamonds')
+    elif card == 24:
+        print('Jack of diamonds')
+    elif card == 25:
+        print('Queen of diamonds')
+    elif card == 26:
+        print('King of diamonds')
+    elif card == 27:
+        print('Ace of clubs')
+    elif card == 28:
+        print('2 of clubs')
+    elif card == 29:
+        print('3 of clubs')
+    elif card == 30:
+        print('4 of clubs')
+    elif card == 31:
+        print('5 of clubs')
+    elif card == 32:
+        print('6 of clubs')
+    elif card == 33:
+        print('7 of clubs')
+    elif card == 34:
+        print('8 of clubs')
+    elif card == 35:
+        print('9 of clubs')
+    elif card == 36:
+        print('10 of clubs')
+    elif card == 37:
+        print('Jack of clubs')
+    elif card == 38:
+        print('Queen of clubs')
+    elif card == 39:
+        print('King of clubs')
+    elif card == 40:
+        print('Ace of hearts')
+    elif card == 41:
+        print('2 of hearts')
+    elif card == 42:
+        print('3 of hearts')
+    elif card == 43:
+        print('4 of hearts')
+    elif card == 44:
+        print('5 of hearts')
+    elif card == 45:
+        print('6 of hearts')
+    elif card == 46:
+        print('7 of hearts')
+    elif card == 47:
+        print('8 of hearts')
+    elif card == 48:
+        print('9 of hearts')
+    elif card == 49:
+        print('10 of hearts')
+    elif card == 50:
+        print('Jack of hearts')
+    elif card == 51:
+        print('Queen of hearts')
+    elif card == 52:
+        print('King of hearts')
+    
     if drawCard is 'yes':
         print(card)
     else:
